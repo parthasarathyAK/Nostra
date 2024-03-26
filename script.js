24a16d1ad39a9bc16fc806ba4f15da51925a900e
parthasarathyAK <sarathypartha631@gmail.com>
2024-03-26 20:28:17 +0530

    nostra

--- script.js
+++ script.js
@@ -1,15 +1,109 @@
-var menu_icon=document.querySelector("#navmenutoggle")
-var offer_bar=document.querySelector(".offerbar")
-var offer_bar_close=document.querySelector("#offerbarclose")
-var side_bar=document.querySelector(".sidebar")
-var side_bar_close=document.querySelector(".sidebarclose")
 
-menu_icon.addEventListener("click",function(){
-    side_bar.style.marginLeft="0";
+var offerBar = document.querySelector(".offer-bar")
+
+document.getElementById("offer-close").addEventListener("click",
+
+function(){
+    offerBar.style.display="none"
+}
+)
+
+var sideNavMenu=document.getElementById("side-navbar-activate")
+var sidenavbar = document.querySelector(".side-navbar")
+sideNavMenu.addEventListener("click",function(){
+    sidenavbar.style.marginLeft="0px"
 })
-side_bar_close.addEventListener("click",function(){
-    side_bar.style.marginLeft="-60%";
+
+document.getElementById("side-navbar-close").addEventListener("click",()=>{
+    document.querySelector(".side-navbar").style.marginLeft = "-60%"
 })
-offer_bar_close.addEventListener("click",function(){
-    offer_bar.style.display="none"
+
+
+
+var sliderleftbutton = document.getElementById("slider-left-activate")
+var sliderrightbutton =document.getElementById("slider-right-activate")
+var sliderimage= document.querySelector(".slider-image-container")
+var slidermargin=0
+
+console.log(sliderleftbutton)
+
+sliderrightbutton.addEventListener("click",
+
+function(){
+    slidermargin=slidermargin+100
+
+    if(slidermargin>200)
+    {
+        slidermargin=0
+        sliderimage.style.marginLeft=0;
+    }
+    else{
+        sliderimage.style.marginLeft="-"+slidermargin+"vw";
+    }
+    
+   
+
+}
+)
+
+sliderleftbutton.addEventListener("click",
+
+function(){
+
+    if(slidermargin==0)
+    {
+       
+        slidermargin=200
+        sliderimage.style.marginLeft="-"+slidermargin+"vw";
+    }
+    else{
+
+        slidermargin=slidermargin-100
+    sliderimage.style.marginLeft="-"+slidermargin+"vw";
+    }
+    
+    
+}
+)
+
+
+
+
+
+var likebuttons=document.querySelectorAll(".like-button")
+
+likebuttons.forEach((btn)=>{
+    btn.addEventListener("click",function(e){
+        
+        
+        if(e.target.src.indexOf("blackheart")>0)
+        {
+            console.log("jkjk");
+            e.target.src="img/icons/redheart.png"
+        }
+        else{
+            e.target.src="img/icons/blackheart.png"
+        }
+    })
 })
+
+window.addEventListener("scroll",function(){
+    var elements = this.document.querySelectorAll(".initial-scroll-animate")
+    elements.forEach((el)=>{
+        windowHeight = window.innerHeight
+     var elbound = el.getBoundingClientRect()
+   
+
+     console.log(windowHeight)
+     console.log(elbound.top)
+     if(windowHeight>elbound.top-100){
+        console.log("Hi")
+    el.classList.remove("reveal-scroll-animate")
+
+    }
+    
+})
+    
+})
+
+
